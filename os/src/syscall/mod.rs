25ac2076b6c8fd4b37cab628ea05324533ba2693
baicheng <danma_0203@qq.com>
2023-12-01 11:37:52 +0800

    时钟中断与计时器
    金柏成
    21301041

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -3,6 +3,7 @@
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
 const SYSCALL_YIELD: usize = 124;
+const SYSCALL_GET_TIME: usize = 169;
 
 mod fs;
 mod process;
@@ -12,6 +13,7 @@ use process::*;
 
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
+        SYSCALL_GET_TIME => sys_get_time(),
         SYSCALL_YIELD => sys_yield(),
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
