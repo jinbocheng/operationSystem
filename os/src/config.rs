0bf5385391a012f99acbbcc80696def5da2b8850
baicheng <danma_0203@qq.com>
2023-12-15 11:27:14 +0800

    物理帧管理测试
    21301041 金柏成

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,11 +1,6 @@
 pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
 
-// os/src/mm/mod.rs
-
-mod heap_allocator;
-
-pub fn init() {
-    heap_allocator::init_heap();
-    heap_allocator::heap_test();
-}
 pub const MEMORY_END: usize = 0x80800000;
+pub const PAGE_SIZE: usize = 0x1000;
+pub const PAGE_SIZE_BITS: usize = 0xc;
+
--- os/src/mm/mod.rs
+++ os/src/mm/mod.rs
@@ -8,4 +8,17 @@ mod page_table;
 
 use page_table::{PTEFlags};
 pub use page_table::{PageTableEntry};
+mod address;
+mod frame_allocator;
+
+use address::{VPNRange, StepByOne};
+pub use address::{PhysAddr, VirtAddr, PhysPageNum, VirtPageNum};
+pub use frame_allocator::{FrameTracker, frame_alloc};
+
+pub fn init() {
+    heap_allocator::init_heap();
+    heap_allocator::heap_test();
+    frame_allocator::init_frame_allocator();
+    frame_allocator::frame_allocator_test();
+}
 
