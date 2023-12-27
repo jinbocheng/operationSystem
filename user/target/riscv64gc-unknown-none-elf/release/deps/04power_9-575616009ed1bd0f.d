6f31fdd87c48cc8dfd6cfdc896408d8400aa9853
baicheng <danma_0203@qq.com>
2023-12-27 17:23:49 +0800

    新应用程序
    21301041 金柏成

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/04power_9-575616009ed1bd0f.d
@@ -0,0 +1,5 @@
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/04power_9-575616009ed1bd0f: src/bin/04power_9.rs
+
+/mnt/user/target/riscv64gc-unknown-none-elf/release/deps/04power_9-575616009ed1bd0f.d: src/bin/04power_9.rs
+
+src/bin/04power_9.rs:
