9ffc1c01cd019ab923a2f62e4f7fc3ca45819350
baicheng <danma_0203@qq.com>
2023-12-27 16:43:14 +0800

    修改main.rs
    21301041 金柏成

--- os/src/main.rs
+++ os/src/main.rs
@@ -45,6 +45,7 @@ pub fn rust_main() -> ! {
     println!("[kernel] Hello, world!");
     mm::init();
     println!("[kernel] back to world!");
+    mm::remap_test();
     trap::init();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
