4fbe734a04e1d620a076f5e56efe283a8d54cf93
baicheng <danma_0203@qq.com>
2023-12-28 17:56:55 +0800

    进程资源回收机制
    21301041 金柏成

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -1,39 +1,28 @@
 mod context;
 mod switch;
 mod task;
+mod manager;
+mod processor;
+mod pid;
 
 use crate::loader::get_app_data_by_name;
-use crate::trap::TrapContext;
-use crate::sync::UPSafeCell;
 use lazy_static::*;
 use switch::__switch;
 use task::{TaskControlBlock, TaskStatus};
-use alloc::vec::Vec;
-use manager::add_task;
+use alloc::sync::Arc;
+use manager::fetch_task;
 
 pub use context::TaskContext;
-
-lazy_static! {
-    pub static ref INITPROC: Arc<TaskControlBlock> = Arc::new(
-        TaskControlBlock::new(get_app_data_by_name("initproc").unwrap())
-    );
-}
-
-pub fn run_first_task() {
-    TASK_MANAGER.run_first_task();
-}
-
-fn run_next_task() {
-    TASK_MANAGER.run_next_task();
-}
-
-fn mark_current_suspended() {
-    TASK_MANAGER.mark_current_suspended();
-}
-
-fn mark_current_exited() {
-    TASK_MANAGER.mark_current_exited();
-}
+pub use processor::{
+    run_tasks,
+    current_task,
+    current_user_token,
+    current_trap_cx,
+    take_current_task,
+    schedule,
+};
+pub use manager::add_task;
+pub use pid::{PidHandle, pid_alloc, KernelStack};
 
 pub fn suspend_current_and_run_next() {
     // There must be an application running.
@@ -53,18 +42,43 @@ pub fn suspend_current_and_run_next() {
     schedule(task_cx_ptr);
 }
 
+pub fn exit_current_and_run_next(exit_code: i32) {
+    // take from Processor
+    let task = take_current_task().unwrap();
+    // **** access current TCB exclusively
+    let mut inner = task.inner_exclusive_access();
+    // Change status to Zombie
+    inner.task_status = TaskStatus::Zombie;
+    // Record exit code
+    inner.exit_code = exit_code;
+    // do not move to its parent but under initproc
 
-pub fn exit_current_and_run_next() {
-    mark_current_exited();
-    run_next_task();
+    // ++++++ access initproc TCB exclusively
+    {
+        let mut initproc_inner = INITPROC.inner_exclusive_access();
+        for child in inner.children.iter() {
+            child.inner_exclusive_access().parent = Some(Arc::downgrade(&INITPROC));
+            initproc_inner.children.push(child.clone());
+        }
+    }
+    // ++++++ release parent PCB
+
+    inner.children.clear();
+    // deallocate user space
+    inner.memory_set.recycle_data_pages();
+    drop(inner);
+    // **** release current PCB
+    // drop task manually to maintain rc correctly
+    drop(task);
+    // we do not have to save task context
+    let mut _unused = TaskContext::zero_init();
+    schedule(&mut _unused as *mut _);
 }
 
-pub fn current_user_token() -> usize {
-    TASK_MANAGER.get_current_token()
-}
-
-pub fn current_trap_cx() -> &'static mut TrapContext {
-    TASK_MANAGER.get_current_trap_cx()
+lazy_static! {
+    pub static ref INITPROC: Arc<TaskControlBlock> = Arc::new(
+        TaskControlBlock::new(get_app_data_by_name("initproc").unwrap())
+    );
 }
 
 pub fn add_initproc() {
