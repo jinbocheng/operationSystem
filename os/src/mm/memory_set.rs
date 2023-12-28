795ef528973e8deba3bb3d4804cda94386fbf6b4
baicheng <danma_0203@qq.com>
2023-12-28 17:31:11 +0800

    在内核栈中保存进程标识符
    21301041 金柏成

--- os/src/mm/memory_set.rs
+++ os/src/mm/memory_set.rs
@@ -48,6 +48,14 @@ impl MemorySet {
             areas: Vec::new(),
         }
     }
+    pub fn remove_area_with_start_vpn(&mut self, start_vpn: VirtPageNum) {
+        if let Some((idx, area)) = self.areas.iter_mut().enumerate()
+            .find(|(_, area)| area.vpn_range.get_start() == start_vpn) {
+            area.unmap(&mut self.page_table);
+            self.areas.remove(idx);
+        }
+    }
+
     pub fn token(&self) -> usize {
         self.page_table.token()
     }
